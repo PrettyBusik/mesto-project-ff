bca08adf73718559b5d29ccb440e28035ebf618e
Marina Pikaeva <marishkapik@yandex.ru>
2024-05-15 09:38:03 +0200

    move cards logic to separate module

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,26 +1,101 @@
-export  const initialCards = [
+import {showPopupCard} from "./popup";
+
+const cardTemplate = document.querySelector('#card-template').content;
+const listOfCards = document.querySelector('.places__list');
+
+
+const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+        name: "Архыз",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+        name: "Челябинская область",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+        name: "Иваново",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+        name: "Камчатка",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+        name: "Холмогорский район",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+        name: "Байкал",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+
+/**
+ *
+ * @param {string} city
+ * @param {string} link
+ */
+export function addCard(city, link) {
+    const newCard = creatCardNode(city, link)
+    listOfCards.prepend(newCard);
+}
+
+function addInitialCards() {
+    initialCards.forEach(function (card) {
+        const newCard = creatCardNode(card.name, card.link)
+        listOfCards.append(newCard);
+    })
+}
+
+/**
+ *
+ * @param {string} city
+ * @param {string} link
+ * @return {Element}
+ */
+function creatCardNode(city, link) {
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__image').src = link;
+    newCard.querySelector('.card__title').textContent = city;
+    return newCard;
+}
+
+function handlerDeletingButton(event) {
+    if (event.target.classList.contains('card__delete-button')) {
+        const cardForRemoving = event.target.closest('.card');
+        cardForRemoving.remove();
+    }
+}
+
+function handlerLike(event) {
+    const likeButton = event.target;
+    if (event.target.classList.contains('card__like-button')) {
+        if (likeButton.classList.contains('card__like-button_is-active')) {
+            likeButton.classList.remove('card__like-button_is-active')
+        } else {
+            likeButton.classList.add('card__like-button_is-active')
+        }
+    }
+}
+
+function handlerCardClick(event) {
+    let cardNode;
+
+    if (event.target.classList.contains('card')) {
+        cardNode = event.target;
+    } else {
+        cardNode = event.target.closest('.card');
+    }
+
+    const imgSrc= cardNode.querySelector('.card__image').src;
+    const titleOfCard= cardNode.querySelector('.card__title').innerText;
+
+    showPopupCard(imgSrc, titleOfCard);
+}
+
+addInitialCards();
+listOfCards.addEventListener('click', handlerDeletingButton);
+listOfCards.addEventListener('click', handlerLike);
+listOfCards.addEventListener('click', handlerCardClick)
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,40 +1,7 @@
-import {initialCards} from "./cards";
 import './../pages/index.css';
 import {showPopupEditing,showPopupAdding,showPopupCard, closeCurrentPopup} from "./popup";
 import {fillEditingForm,subscribeToEditingFormSubmitting, subscribeToAddingFormSubmitting} from "./forms";
-
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
-
-// @todo: DOM узлы
-const listOfCards = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-function createCard(card, removingCard, like) {
-    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
-    newCard.querySelector('.card__image').src = card.link;
-    newCard.querySelector('.card__title').textContent = card.name;
-
-
-    const deleteButton = newCard.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', removingCard);
-    return newCard;
-}
-
-// @todo: Функция удаления карточки
-function removeCard(event) {
-    const eventTarget = event.target;
-    const cardForRemoving = eventTarget.closest('.card');
-    cardForRemoving.remove();
-}
-
-// @todo: Вывести карточки на страницу
-initialCards.forEach(function (card) {
-    const newCard = createCard(card, removeCard);
-    listOfCards.append(newCard)
-})
-
-
+import {addCard} from "./cards";
 
 
 const editButton = document.querySelector('.profile__edit-button');
@@ -45,13 +12,6 @@ editButton.addEventListener('click', function () {
 
 const addButton = document.querySelector('.profile__add-button');
 addButton.addEventListener('click',  showPopupAdding);
-
-const cardImg = document.querySelector('.card__image');
-const popupImg = document.querySelector('.popup_type_image');
-// cardImg.addEventListener('click', () => showPopup(popupImg));
-
-
-
 
 const nameOfUserInPage = document.querySelector('.profile__title');
 const descriptionOfUserInPage = document.querySelector('.profile__description');
@@ -73,25 +33,12 @@ function handlerFormSubmitForAddingCard(cityName, imgLink) {
         name: cityName,
         link: imgLink
     }
-    const cardForAdding = createCard(newCard, removeCard);
-    listOfCards.prepend(cardForAdding);
+    addCard(newCard.name, newCard.link);
+
     closeCurrentPopup();
 }
 
 subscribeToAddingFormSubmitting(handlerFormSubmitForAddingCard);
 
-function like(event) {
-    const likeButton = event.target;
-    if (event.target.classList.contains('card__like-button_is-active')) {
-        likeButton.classList.remove('card__like-button_is-active')
-    } else {
-        likeButton.classList.add('card__like-button_is-active')
-    }
-}
 
-listOfCards.addEventListener('click', function (event) {
-    if (event.target.classList.contains('card__like-button')) {
-        like(event)
-    }
-})
 
